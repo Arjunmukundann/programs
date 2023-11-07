c28107a5ad9255a55713a271958537242b31207a
Arjunmukundann <144939625+Arjunmukundann@users.noreply.github.com>
2023-11-07 20:21:49 +0530

    Task.C

---
+++ Task.C
@@ -0,0 +1,39 @@
+Programming Task
+Create a program that computes the average marks of a student.
+
+Create an array that stores the marks of 5 subjects
+Compute the total marks by adding all the marks
+Divide the total marks by total number to subjects
+Print the average marks
+Solution :
+#include <stdio.h>
+
+int main() {
+    int i;
+    double marks[5];
+    printf("Enter marks:\n");
+    
+    for(i = 0; i < 5; i++) {
+        scanf("%lf", &marks[i]);
+    }
+    
+    double sum = 0;
+    for(i = 0; i < 5; i++) {
+        sum = marks[i] + sum;
+    }
+    
+    double avg_marks = sum/5;
+    printf("The average marks of a student: %.2lf", avg_marks);
+    
+    return 0;
+}
+Output
+
+Enter marks:
+1
+2
+3
+4
+5
+The average marks of a student: 3.00
+  
